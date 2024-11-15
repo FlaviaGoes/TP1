64b2d23c796cfbc93354d2f752fe1a9e7ca3ab99
FlaviaGoes <flavia04goes@outlook.com>
2024-11-14 22:25:14 -0300

    first commit

---
+++ Emporio/Pao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace $safeprojectname$
+{
+    internal class Pao : Produto
+    {
+        public double aumento = 0.015;
+        public Pao(int id, String nome, String descricao, double preco) : base(id, nome, descricao, preco)
+        {
+        }
+
+        public override double ReajustarPreco()
+        {
+            Preco = Preco * (1 + aumento / 100);
+            return Preco;
+        }
+    }
+}
---
+++ Emporio/Produto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace $safeprojectname$
+{
+    abstract class Produto
+    {
+        public int Id { get; set; }
+        public String Nome { get; set; }
+        public String Descricao {  get; set; }
+        public double Preco {  get; set; }
+
+        public Produto(int id, String nome, String descricao, double preco) {
+            this.Id = id;
+            this.Nome = nome;
+            this.Descricao = descricao;
+            this.Preco = preco;
+        }
+
+        public virtual void ExibirProduto()
+        {
+            Console.WriteLine($"Id: {Id} Nome: {Nome} Descricao: {Descricao} Valor: {Valor}");
+        }
+
+        public abstract double ReajustarPreco();
+
+    }
+}
