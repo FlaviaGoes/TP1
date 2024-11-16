9afda8d9a31c6f5188fae60db00af50e3735c806
Flavia Moreira Goes <133283900+FlaviaGoes@users.noreply.github.com>
2024-11-16 00:08:28 -0300

    Inserção dos métodos
    Infelizmente tive que apagar a versão anterior e adicionar o final da atividade pois não sabia como utilizar o git no visual studio.

---
+++ Emporio/CestadeCompras.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace $safeprojectname$
+{
+    internal class CestadeCompras
+    {
+        private static int proximoId = 1;
+        public String Usuario { get; set; }
+        public int Id { get; private set; }
+        public double Total { get; set; }
+        public List<Produto> ListaDeProdutos { get; set; }
+
+        public CestadeCompras(String usuario)
+        {
+            this.Id = proximoId;
+            proximoId++;
+            this.Usuario = usuario;
+            this.ListaDeProdutos = new List<Produto>();
+        }
+
+        public void AdicionarProduto(Produto produto)
+        {
+            ListaDeProdutos.Add(produto);
+        }
+
+        public void ExibirProdutos()
+        {
+            foreach (Produto p in ListaDeProdutos)
+            {
+                Console.WriteLine(p.Nome);
+                Console.WriteLine(p.Preco);
+            }
+        }
+
+        public double ExibirPreco()
+        {
+            Total = 0;
+            foreach (Produto p in ListaDeProdutos)
+            {
+                Total += p.Preco;
+            }
+
+            return Total;
+        }
+
+        public void Reajuste()
+        {
+            foreach (Produto p in ListaDeProdutos)
+            {
+                p.ReajustarPreco();
+                p.ExibirProduto();
+            }
+
+        }
+
+    }
+}
---
+++ Emporio/Produto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace $safeprojectname$
+{
+    abstract class Produto
+    {
+        private static int proximoId = 1;
+        public int Id { get; private set; }
+        public String Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        public Produto(String nome, double preco, int quantidade)
+        {
+            this.Id = proximoId;
+            proximoId++;
+            this.Nome = nome;
+            this.Preco = preco;
+            this.Quantidade = quantidade;
+        }
+
+        public virtual void ExibirProduto()
+        {
+            Console.WriteLine($"Id: {Id} Nome: {Nome}");
+            Console.WriteLine($"Quantidade: {Quantidade} Valor: {Preco}\n");
+        }
+
+        public abstract double ReajustarPreco();
+
+    }
+}
